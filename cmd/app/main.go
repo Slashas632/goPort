dd91361aa437fcc07d43363efad82326ea558b1d
Slešas <49253505+Emilijus052@users.noreply.github.com>
2026-04-17 17:52:14 +0000

    Added structure

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,40 +2,10 @@ package main
 
 import (
 	"port-scanner/internal/cli"
+	"port-scanner/internal/scanner"
 )
 
-/*
-func ip_scanner() {
-
-		// IP
-
-		var ip string
-		fmt.Println("Enter IP which you want to scan: ")
-		fmt.Scan(&ip)
-		ip_parts := strings.Split(ip, ".")
-
-		// Diapazonas
-
-		var dp string
-		fmt.Println("Pasirinkite diapazoną kurį norite nuskanuoti")
-		fmt.Scan(&dp)
-
-		diapazonas := strings.Split(dp, "-")
-		start, _ := strconv.Atoi(diapazonas[0])
-		end, _ := strconv.Atoi(diapazonas[1])
-
-		for start < end {
-			prefix := ip_parts[0] + "." + ip_parts[1] + "." + ip_parts[2] + "."
-
-			pilnas_ip := prefix + "." + strconv.Itoa(start)
-
-			start++
-		}
-
-		fmt.Println("Your ip is: ", ip_parts[3])
-		fmt.Println(prefix)
-	}
-*/
 func main() {
-	cli.Arguments()
+	opts := cli.ParseArgs()
+	scanner.Run(opts)
 }
--- internal/cli/args.go
+++ internal/cli/args.go
@@ -1,11 +1,25 @@
 package cli
 
 import (
-	"fmt"
-	"os"
+	"flag"
 )
 
-func Arguments() {
-	protocol := os.Args[1]
-	fmt.Println(protocol)
+type Options struct {
+	TCP  bool
+	UDP  bool
+	Port int
+}
+
+func ParseArgs() Options {
+	tcp := flag.Bool("tcp", false, "TCP scan")
+	udp := flag.Bool("udp", false, "UDP scan")
+	port := flag.Int("p", 80, "port")
+
+	flag.Parse()
+
+	return Options{
+		TCP:  *tcp,
+		UDP:  *udp,
+		Port: *port,
+	}
 }
