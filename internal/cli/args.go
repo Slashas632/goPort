3bec0ecda2d85173d42ef3887ae2b2db65ffcab7
Slešas <49253505+Emilijus052@users.noreply.github.com>
2026-04-20 19:21:20 +0000

    Started first protocol

--- internal/cli/args.go
+++ internal/cli/args.go
@@ -7,12 +7,16 @@ import (
 type Options struct {
 	TCP  bool
 	UDP  bool
+	IPS  bool
+	IP   string
 	Port int
 }
 
 func ParseArgs() Options {
 	tcp := flag.Bool("tcp", false, "TCP scan")
 	udp := flag.Bool("udp", false, "UDP scan")
+	ips := flag.Bool("ips", false, "IP scan")
+	ip := flag.String("ip", "127.0.0.1", "IP adress")
 	port := flag.Int("p", 80, "port")
 
 	flag.Parse()
@@ -21,5 +25,7 @@ func ParseArgs() Options {
 		TCP:  *tcp,
 		UDP:  *udp,
 		Port: *port,
+		IPS:  *ips,
+		IP:   *ip,
 	}
 }
--- internal/scanner/engine.go
+++ internal/scanner/engine.go
@@ -2,41 +2,11 @@ package scanner
 
 import (
 	"port-scanner/internal/cli"
+	"port-scanner/internal/protocols"
 )
 
 func Run(opts cli.Options) {
-
-}
-
-/*
-func ip_scanner() {
-
-		// IP
-
-		var ip string
-		fmt.Println("Enter IP which you want to scan: ")
-		fmt.Scan(&ip)
-		ip_parts := strings.Split(ip, ".")
-
-		// Diapazonas
-
-		var dp string
-		fmt.Println("Pasirinkite diapazoną kurį norite nuskanuoti")
-		fmt.Scan(&dp)
-
-		diapazonas := strings.Split(dp, "-")
-		start, _ := strconv.Atoi(diapazonas[0])
-		end, _ := strconv.Atoi(diapazonas[1])
-
-		for start < end {
-			prefix := ip_parts[0] + "." + ip_parts[1] + "." + ip_parts[2] + "."
-
-			pilnas_ip := prefix + "." + strconv.Itoa(start)
-
-			start++
-		}
-
-		fmt.Println("Your ip is: ", ip_parts[3])
-		fmt.Println(prefix)
+	if opts.IPS {
+		protocols.Ipscanner(opts.IP)
 	}
-*/
+}
