e7492c39e5ed3501afe977e517545efba33ebab0
Slešas <49253505+Emilijus052@users.noreply.github.com>
2026-05-01 17:50:50 +0000

    Finally added simple TCP scan

--- internal/protocols/TCP.go
+++ internal/protocols/TCP.go
@@ -2,8 +2,19 @@ package protocols
 
 import (
 	"fmt"
+	"net"
+	"strconv"
+	"time"
 )
 
 func Tcp(port int, ip string) {
-	fmt.Println(port, ip)
+	full_ip := ip + ":" + strconv.Itoa(port)
+	timeout := time.Second * 2
+	conn, err := net.DialTimeout("tcp", full_ip, timeout)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	fmt.Printf("Port Open %s\n", full_ip)
 }
