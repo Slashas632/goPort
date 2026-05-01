12708911c51171e33823a0de02ac13c0a0455e27
Slešas <49253505+Emilijus052@users.noreply.github.com>
2026-05-01 19:03:21 +0000

    Added Banner Grabbing for TCP

--- internal/protocols/TCP.go
+++ internal/protocols/TCP.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -16,5 +17,29 @@ func Tcp(port int, ip string) {
 	}
 	defer conn.Close()
 
-	fmt.Printf("Port Open %s\n", full_ip)
+	conn.SetReadDeadline(time.Now().Add(timeout))
+
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+		conn.SetReadDeadline(time.Now().Add(timeout))
+		n, err = conn.Read(buf)
+		if err != nil {
+			return
+		}
+	}
+
+	banner := strings.TrimSpace(string(buf[:n]))
+	firstLine := strings.SplitN(banner, "\n", 2)[0]
+
+	const (
+		Green  = "\033[32m"
+		Yellow = "\033[33m"
+		Cyan   = "\033[36m"
+		Reset  = "\033[0m"
+	)
+
+	fmt.Printf("%s[OPEN]%s %s | %s%s%s\n", Green, Reset, full_ip, Yellow, firstLine, Reset)
+
 }
